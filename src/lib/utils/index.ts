3dd55d3504a438436f61c3d3ace12877458c5e55
cain.chu <cainmaila>
2024-05-29 14:12:21 +0800

    添加Axios攔截器

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -5,19 +5,19 @@ import { goto } from '$app/navigation'
  * 將授權標題添加到Axios請求
  */
 export function axiosInterceptors() {
-    const accessToken = Cookies.get('accessToken')
-    const interceptor = axios.interceptors.request.use((request) => {
-        switch (request.url) {
-            case '/auth/users/login':
-                return request
-        }
-        accessToken && (request.headers.Authorization = `Bearer ${accessToken}`)
-        return request
-    })
-    if (!accessToken || accessToken === 'undefined') {
-        goto('/login', { replaceState: true })
-    }
-    return () => {
-        axios.interceptors.request.eject(interceptor)
-    }
+	const accessToken = Cookies.get('accessToken')
+	const interceptor = axios.interceptors.request.use((request) => {
+		switch (request.url) {
+			case '/auth/users/login':
+				return request
+		}
+		accessToken && (request.headers.Authorization = `Bearer ${accessToken}`)
+		return request
+	})
+	if (!accessToken || accessToken === 'undefined') {
+		goto('/login', { replaceState: true })
+	}
+	return () => {
+		axios.interceptors.request.eject(interceptor)
+	}
 }
