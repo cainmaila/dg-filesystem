c53a1cbede905f3ecbe951f69c4500ff4e620121
cain.chu <cainmaila>
2024-05-29 14:57:35 +0800

    sdk

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -6,25 +6,25 @@ import { page } from '$app/stores'
  * 將授權標題添加到Axios請求
  */
 export function axiosInterceptors() {
-    page.subscribe(({ data }) => {
-        const { INVENTEC_SERVICE_URL } = data
-        axios.defaults.baseURL = INVENTEC_SERVICE_URL || ''
-    })
-    const interceptor = axios.interceptors.request.use((request) => {
-        switch (request.url) {
-            case '/auth/users/login':
-                return request
-        }
-        const accessToken = Cookies.get('accessToken')
-        if (!accessToken || accessToken === 'undefined') {
-            goto('/login', { replaceState: true })
-        }
-        accessToken && (request.headers.Authorization = `Bearer ${accessToken}`)
-        request.headers['Access-Control-Allow-Headers'] = 'Authorization'
-        return request
-    })
+	page.subscribe(({ data }) => {
+		const { INVENTEC_SERVICE_URL } = data
+		axios.defaults.baseURL = INVENTEC_SERVICE_URL || ''
+	})
+	const interceptor = axios.interceptors.request.use((request) => {
+		switch (request.url) {
+			case '/auth/users/login':
+				return request
+		}
+		const accessToken = Cookies.get('accessToken')
+		if (!accessToken || accessToken === 'undefined') {
+			goto('/login', { replaceState: true })
+		}
+		accessToken && (request.headers.Authorization = `Bearer ${accessToken}`)
+		request.headers['Access-Control-Allow-Headers'] = 'Authorization'
+		return request
+	})
 
-    return () => {
-        axios.interceptors.request.eject(interceptor)
-    }
+	return () => {
+		axios.interceptors.request.eject(interceptor)
+	}
 }
